afb1d786225adb2fcaf301a009898c0985bcf9f6
Jolan-Goulin <77050754+Jolan-Goulin@users.noreply.github.com>
2021-05-28 10:39:01 +0200

    Add files via upload

---
+++ si proj v2/lecture.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <main.h>
+using namespace std;
+
+vector<vector<int>> readhm(string(nomfichierhm));
+void fetchData(tData* tdata) ;
